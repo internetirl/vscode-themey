2a407899b4c83ebb669a401d09db8d74961bbf1b
internetirl <mattferri@gmail.com>
2018-08-23 02:39:48 -0400

    Initial commit for Themey

---
+++ src/extension.ts
@@ -0,0 +1,45 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as themeGenerator from './themeGenerator';
+
+function promptToReloadWindow() {
+  const action = 'Reload';
+
+  vscode.window
+    .showInformationMessage(
+      `Reload window to see updated theme. Make sure to have the 'Themey' theme selected.`,
+      action
+    )
+    .then(selectedAction => {
+      if (selectedAction === action) {
+        vscode.commands.executeCommand('workbench.action.reloadWindow');
+      }
+    });
+}
+
+export function activate(context: vscode.ExtensionContext) {
+    let disposable = vscode.commands.registerCommand('extension.runThemey', () => {
+        let inputOpts = {
+            placeHolder: 'Specify location of image',
+            prompt: 'Specify location of image',
+            ignoreFocusOut: true
+        };
+        vscode.window.showInputBox(inputOpts).then((imageUrl) => {
+            if(!imageUrl || imageUrl === '') {
+              return;
+            }
+
+            let location = path.join(__dirname, '..', 'themes', 'themey.json');
+            themeGenerator.generateThemesFromImage(imageUrl.trim(), location, (err: any, message: any) => {
+              if(err) {
+                 vscode.window.showInformationMessage("Error getting palette from image: " + err); 
+              } else {
+                 promptToReloadWindow();
+              }
+            });
+        });
+    });
+    context.subscriptions.push(disposable);
+}
---
+++ src/themeGenerator.ts
@@ -0,0 +1,54 @@
+import * as path from 'path';
+import {
+    generateTheme,
+    IColorSet
+} from 'vscode-theme-generator';
+import Vibrant = require('node-vibrant');
+
+export interface Callback<T> {
+    (err?: Error, result?: T): void;
+}
+
+export function generateThemesFromImage(image: string, location: string, cb?: Callback<string>) {
+
+    let vibrant: string;
+    let lightVibrant: string;
+    let darkVibrant: string;
+    let muted: string;
+    let lightMuted: string;
+    let darkMuted: string;
+
+    const defaultColor: string = '#000000';
+
+    Vibrant.from(image).getPalette((err: any, palette: any) => {
+        if (err && cb) {
+            cb(err, undefined);
+        }
+
+        vibrant      = palette.Vibrant      ? palette.Vibrant.getHex()      : defaultColor;
+        lightVibrant = palette.LightVibrant ? palette.LightVibrant.getHex() : defaultColor;
+        darkVibrant  = palette.DarkVibrant  ? palette.DarkVibrant.getHex()  : defaultColor;
+        muted        = palette.Muted        ? palette.Muted.getHex()        : defaultColor;
+        lightMuted   = palette.LightMuted   ? palette.LightMuted.getHex()   : defaultColor;
+        darkMuted    = palette.DarkMuted    ? palette.DarkMuted.getHex()    : defaultColor;
+        darkMuted = "#1e1e1e";
+
+        let colorSet: IColorSet = {
+            base: {
+                background: darkMuted || defaultColor,
+                foreground: lightMuted || defaultColor,
+                color1: muted || defaultColor,
+                color2: vibrant || defaultColor,
+                color3: vibrant || defaultColor,
+                color4: lightVibrant || defaultColor
+            }
+        };
+
+        let themeName = 'Themey';
+        generateTheme(themeName, colorSet, path.join(__dirname, '..', 'themes', 'themey.json'));
+        
+        if(cb) {
+            cb(undefined, "Successfully created themes.");
+        }
+    });
+}
